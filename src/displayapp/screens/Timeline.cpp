3d617c386db59d03f38a236bf7c43078166d7b18
Cole Wilson <cole@colewilson.xyz>
2022-06-21 13:16:18 -0700

    [timeline] add menu

--- src/displayapp/screens/Timeline.cpp
+++ src/displayapp/screens/Timeline.cpp
@@ -4,18 +4,37 @@
 using namespace Pinetime::Applications::Screens;
 
 Timeline::Timeline(DisplayApp* app, Controllers::MotorController& motorController, Controllers::DateTime& dateTimeController, Controllers::FS& fsController) : Screen(app) {
-  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "+"+ReadFile()+"+");
-  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
-  lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
-}
-char* Timeline::ReadFile() {
-	char* buffer;
-	lfs_file_t calFile;
-	fs.FileOpen(&calFile, calFileName, LFS_O_RDONLY | LFS_O_CREAT);
-	fs.FileRead(&calFile, reinterpret_cast<char*>(&buffer), 100);
-	fs.FileClose(&calFile);
-	return buffer;
+  lv_obj_t * menu = lv_menu_create(lv_scr_act());
+    lv_obj_set_size(menu, lv_disp_get_hor_res(nullptr), lv_disp_get_ver_res(nullptr));
+    lv_obj_center(menu);
+
+    lv_obj_t * cont;
+    lv_obj_t * label;
+
+    /*Create a sub page*/
+    lv_obj_t * sub_page = lv_menu_page_create(menu, nullptr);
+
+    cont = lv_menu_cont_create(sub_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
+
+    /*Create a main page*/
+    lv_obj_t * main_page = lv_menu_page_create(menu, nullptr);
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 1");
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 2");
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 3 (Click me!)");
+    lv_menu_set_load_page_event(menu, cont, sub_page);
+
+    lv_menu_set_page(menu, main_page);
 }
 
 Timeline::~Timeline() { // destroy
