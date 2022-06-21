2806ff04366a299992ef8ef0d20e2a81fff08af4
Cole Wilson <cole@colewilson.xyz>
2022-06-21 08:31:40 -0700

    Update Timeline.cpp

--- src/displayapp/screens/Timeline.cpp
+++ src/displayapp/screens/Timeline.cpp
@@ -4,10 +4,17 @@
 using namespace Pinetime::Applications::Screens;
 
 Timeline::Timeline(DisplayApp* app, Controllers::MotorController& motorController, Controllers::DateTime& dateTimeController) : Screen(app) {
-  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "My test application");
-  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
-  lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+    lv_obj_t * list1 = lv_list_create(lv_scr_act(), nullptr);
+    lv_obj_set_size(list1, 160, 200);
+    lv_obj_align(list1, nullptr, LV_ALIGN_CENTER, 0, 0);
+
+    /*Add buttons to the list*/
+    lv_obj_t * list_btn;
+
+    for (int i=0;i<100;i++)
+      list_btn = lv_list_add_btn(list1, nullptr, "event!");
+//     lv_obj_set_event_cb(list_btn, event_handler);
+
 }
 
 Timeline::~Timeline() { // destroy
