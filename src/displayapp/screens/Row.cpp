08e5b1b75bb334c679df21dc1f62b0c66f9131c9
Cole Wilson <cole@colewilson.xyz>
2022-07-14 16:01:06 -0700

    clock

--- src/displayapp/screens/Row.cpp
+++ src/displayapp/screens/Row.cpp
@@ -27,25 +27,25 @@ Row::Row(DisplayApp* app,
 	lv_obj_set_style_local_bg_color(lv_scr_act(), LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x5C7162));
 
 	lv_obj_t* timeofday = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_set_style_local_text_font(timeofday, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_48);
+	/* lv_obj_set_style_local_text_font(timeofday, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_48); */
 	lv_label_set_text(timeofday, "12:34 PM");
 	lv_label_set_align(timeofday, LV_LABEL_ALIGN_CENTER);
 	lv_obj_align(timeofday, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 10);
 
 	lv_obj_t* strokerate = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_set_style_local_text_font(strokerate, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_76);
+	/* lv_obj_set_style_local_text_font(strokerate, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_76); */
 	lv_label_set_text(strokerate, "28");
 	lv_label_set_align(strokerate, LV_LABEL_ALIGN_CENTER);
 	lv_obj_align(strokerate, NULL, LV_ALIGN_IN_LEFT_MID, 10, -10);
 
 	lv_obj_t* timer = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_set_style_local_text_font(timer, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_76);
+	/* lv_obj_set_style_local_text_font(timer, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_76); */
 	lv_label_set_text(timer, "06:27.89");
 	lv_label_set_align(timer, LV_LABEL_ALIGN_CENTER);
 	lv_obj_align(timer, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 10, -10);
 
 	lv_obj_t* strokecount = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_set_style_local_text_font(strokecount, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_48);
+	/* lv_obj_set_style_local_text_font(strokecount, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_48); */
 	lv_label_set_text(strokecount, "220");
 	lv_label_set_align(strokecount, LV_LABEL_ALIGN_CENTER);
 	lv_obj_align(strokecount, NULL, LV_ALIGN_IN_RIGHT_MID, -10, -10);
