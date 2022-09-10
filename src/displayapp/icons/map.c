fffbfd6519694f2d91698a880a6ef73794f98653
Cole Wilson <cole@colewilson.xyz>
2022-09-10 11:26:42 -0700

    .

--- src/displayapp/icons/map.c
+++ src/displayapp/icons/map.c
@@ -9,23 +9,23 @@
 #define LV_ATTRIBUTE_MEM_ALIGN
 #endif
 
-#ifndef LV_ATTRIBUTE_IMG_BG_CLOCK
-#define LV_ATTRIBUTE_IMG_BG_CLOCK
+#ifndef LV_ATTRIBUTE_IMG_MAP
+#define LV_ATTRIBUTE_IMG_MAP
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BG_CLOCK uint8_t bg_clock_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_MAP uint8_t bg_clock_map[] = {
   0x00, 0x00, 0x00, 0x00, 	/*Color of index 0*/
   0x00, 0x00, 0x00, 0x00, 	/*Color of index 1*/
 
   0x00,
 };
 
-const lv_img_dsc_t bg_clock = {
+const lv_img_dsc_t map = {
   .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 1,
   .header.h = 1,
   .data_size = 9,
-  .data = bg_clock_map,
+  .data = map_map,
 };
